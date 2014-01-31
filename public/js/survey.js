6db190a51fb1551e57d105eebb51492585b26553
Harihar Subramanyam <hsubrama@mit.edu>
2014-01-31 16:39:33 -0500

    asset folder

---
+++ public/js/survey.js
@@ -0,0 +1,16 @@
+var video_element;
+var question_div;
+var survey_manager;
+
+$(document).ready(function(){
+    video_element = document.getElementById("video_element");
+    question_div = document.getElementById("question_div");
+    $.getJSON("../json/survey.json", function(survey_json){
+        survey_manager = new SurveyManager({
+            "videoElement":video_element,
+            "questionDiv":question_div,
+            "surveyVideos":survey_json
+        });
+        survey_manager.displayNextVideo();
+    });
+});
